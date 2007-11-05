17e665ea82709a271cf94d3308ee92abf35a79a1
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-11-05 02:02:55 +0000

    Added multithreading.
    git-svn-id: http://svn.winnow.peerworks.org/trunk/classifier_c@2067 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
+#include <pthread.h>
 #include "item.h"
 #include "pool.h"
 #include "random_background.h"
@@ -16,7 +17,7 @@
 #include "tag.h"
 #include "classifier.h"
 
-void do_classification(void);
+void do_classification(void *nothing);
 
 ItemSource is;
 Samples samples;
@@ -24,11 +25,29 @@ Pool random_background;
 TagList tags;
 int num_tags;
 int current_tag;
-float training_clocks = 0;
-float precomputing_clocks = 0;
-float classification_clocks = 0;
+int threads_that_did_work = 0;
+double training = 0;
+double precomputing = 0;
+double classification = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+double now() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return t.tv_sec + (t.tv_usec / 1000000.0);
+}
 
 int main(int argc, char ** argv) {
+  int i;
+  int num_threads = 1;
+  if (argc == 2) {
+    int new_num_threads;
+    if (1 == sscanf(argv[1], "%d", &new_num_threads)) {
+      printf("Using %d threads\n", new_num_threads);
+      num_threads = new_num_threads;
+    }
+  }
+  
   is = create_file_item_source("corpus");
   random_background = create_random_background_from_file(is, "corpus/random_background.txt");
   printf("About to load samples - this could take a while.\n");
@@ -41,19 +60,36 @@ int main(int argc, char ** argv) {
   current_tag = 1;
   printf("Loaded %d tags\n", num_tags);
  
-  do_classification();
+  double start = now();
+  pthread_t threads[num_threads];
+  for (i = 0; i < num_threads; i++) {
+    pthread_create(&threads[i], NULL, do_classification, NULL);
+  }
   
-  printf("Training:       %.2f s, %.1f tags trained per second\n", 
-    training_clocks / CLOCKS_PER_SEC, (float)num_tags / (training_clocks / CLOCKS_PER_SEC));
-  printf("Precomputing:   %.2f s, %.1f precomputations per second\n",
-    precomputing_clocks / CLOCKS_PER_SEC, (float)num_tags / (precomputing_clocks / CLOCKS_PER_SEC));
-  printf("Classification: %.2f s, %.0f classifications per second\n", 
-    classification_clocks / CLOCKS_PER_SEC, ((float)sample_size * num_tags) / (classification_clocks / CLOCKS_PER_SEC));
+  for (i = 0; i < num_threads; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  double end = now();
+  
+  printf("Total (includes thread creation and memory management):\n");
+  printf("                %.2f s, %7.0f classifications / second\n",
+        end - start,  (sample_size * num_tags) / (end - start));
+  
+  training = training / threads_that_did_work;
+  precomputing = precomputing / threads_that_did_work;
+  classification = classification / threads_that_did_work;    
+  
+  printf("Training:       %.2f s, %7.1f    tags trained / second\n", 
+    training, (float)num_tags / (training));
+  printf("Precomputing:   %.2f s, %7.1f precomputations / second\n",
+    precomputing, (float)num_tags / (precomputing));
+  printf("Classification: %.2f s, %7.0f classifications / second\n", 
+    classification, 
+    ((float)sample_size * num_tags) / (classification));
   
   free_taglist(tags);
   free_pool(random_background);
   const Item *items = sample_items(samples);
-  int i;
   
   for (i = 0; i < sample_size; i++) {
     free_item(items[i]);
@@ -64,32 +100,51 @@ int main(int argc, char ** argv) {
   return 0;
 }
 
-void do_classification() {
-  for (; current_tag <= num_tags; current_tag++) {
+void do_classification(void *nothing) {
+  int did_work = 0;
+  double _training = 0;
+  float _precomputing = 0;
+  float _classification = 0;
+  
+  while (current_tag <= num_tags) {
+    did_work = 1;
+    pthread_mutex_lock(&mutex);
     Tag tag = taglist_tag_at(tags, current_tag);
-    clock_t train_start = clock();
+    current_tag++;
+    pthread_mutex_unlock(&mutex);
+    
+    double train_start = now();
     TrainedClassifier trained = train(tag, is);
-    clock_t train_end = clock();
-    training_clocks += (train_end - train_start);
+    double train_end = now();
+    _training += (train_end - train_start);
     
     Classifier classifier = precompute(trained, random_background);
-    clock_t precompute_end = clock();
-    precomputing_clocks += (precompute_end - train_end);
+    double precompute_end = now();
+    _precomputing += (precompute_end - train_end);
     
     const Item *items = sample_items(samples);
     const int sample_size = sample_size(samples);
     int i;
 
-    clock_t cls_start = clock();
+    double cls_start = now();
     for (i = 0; i < sample_size; i++) {
       Tagging tagging = classify(classifier, items[i]);
       //fprintf(stderr, "%s,%i,%.9f\n", tagging->tag_name, item_get_id(items[i]), tagging->strength);    
       free(tagging);
     }
-    clock_t cls_end = clock();
-    classification_clocks += (cls_end - cls_start);
+    double cls_end = now();
+    _classification += (cls_end - cls_start);
   
     free_classifier(classifier);  
     tc_free(trained);
+  }
+  
+  if (did_work) {
+    pthread_mutex_lock(&mutex);
+      threads_that_did_work++;
+      training += _training;
+      precomputing += _precomputing;
+      classification += _classification;    
+    pthread_mutex_unlock(&mutex);
   }
 }
