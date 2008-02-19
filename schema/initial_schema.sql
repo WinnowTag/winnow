2eedb30e6e5164b1cd12a62c82bcfbd01ec7c701
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-02-19 05:01:58 +0000

    New initial schema file for item cache.
    git-svn-id: http://svn.winnow.peerworks.org/branches/classifier_c/item_cache_ticket600@2950 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

---
+++ schema/initial_schema.sql
@@ -0,0 +1,42 @@
+-- This schema uses Atom 1.0 nomenclature --
+begin;
+
+pragma user_version = 1;
+
+create table "feeds" (
+  "id"          integer NOT NULL PRIMARY KEY,
+  "title"       text
+);
+  
+create table "entry" (
+  "id"          integer NOT NULL PRIMARY KEY,
+  "full_id"     text,
+  "title"       text,
+  "author"      text,
+  "alternate"   text,
+  "self"        text,
+  "content"     text,
+  "updated"     text,
+  "feed_id"     integer NOT NULL,
+  "created_at"  text,
+  constraint "entry_feed" foreign key ("feed_id")
+    references "feeds" ("id") ON DELETE CASCADE
+);
+  
+create table "tokens" (
+  "id"       integer NOT NULL PRIMARY KEY,
+  "token"    text    NOT NULL UNIQUE
+);
+  
+create table "entry_tokens" (
+  "entry_id" integer NOT NULL,
+  "token_id"     integer NOT NULL,
+  "frequency"    integer NOT NULL,
+  primary key ("entry_id", "token_id"),
+  constraint "entry_tokens_entry_id" foreign key ("entry_id")
+    references "feed_itemss" ("id") ON DELETE CASCADE,
+  constraint "entry_tokens_token_id" foreign key ("token_id")
+    references "tokens" ("id")
+);
+
+commit;
