6f2b4c3a6f6189d65973f28f8f5ff39039e2ece0
Sean Geoghegan <seangeo@gmail.com>
2008-05-02 13:56:09 +0930

    Support multiple requests in http tests.

--- tests/test_http_server.rb
+++ tests/test_http_server.rb
@@ -7,10 +7,72 @@
 
 require 'webrick'
 
+# Monkey patch WEBrick's ProcHandler so that it forwards on
+# PUT and DELETE requests.
+#
+class WEBrick::HTTPServlet::ProcHandler
+  alias do_PUT do_GET
+  alias do_DELETE do_GET
+end
+
 def receive_request(path, timeout = 2, &block)
   HttpRequestMatcher.new(path, timeout, block)
 end
 
+def receive_requests(timeout = 2)
+  matcher = MultipleHttpRequestMatcher.new(timeout)
+  yield(matcher)
+  matcher
+end
+
+class MultipleHttpRequestMatcher
+  def initialize(timeout)
+    @timeout = timeout
+    @requests = {}
+    @request_paths = []
+    @received_requests = []
+  end
+  
+  def request(path, &block)
+    @request_paths << path
+    @requests[path] = block
+  end
+  
+  def matches?(http)
+    @received_requests.extend(MonitorMixin)
+    cond = @received_requests.new_cond
+    exception = nil
+    
+    http.start do |server|
+      @requests.each do |path, block|
+        server.mount_proc(path) do |req, res|
+          begin
+            block.call(req, res) if block
+          rescue Exception => e
+            exception = e
+          end
+          
+          @received_requests.synchronize do
+            @received_requests << path
+            cond.signal if @received_requests.size == @requests.size
+          end
+        end
+      end
+    end
+    
+    @received_requests.synchronize do
+      cond.wait(@timeout) unless @received_requests.size == @requests.size
+      http.shutdown
+    end
+    
+    exception and raise(exception) or @received_requests == @request_paths
+  end
+  
+  def failure_message
+    "Expected requests to [#{@request_paths.join(', ')}] got [#{@received_requests.join('m')}] after #{@timeout}s."
+  end
+end
+  
 class HttpRequestMatcher
   def initialize(path, timeout, block)
     @path = path
