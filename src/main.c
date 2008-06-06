8cdda2b40089a8a8f56a6be319694398dcc45696
Sean Geoghegan <seangeo@gmail.com>
2008-06-06 11:04:38 +0930

    Fixed formatting in help

--- src/main.c
+++ src/main.c
@@ -66,11 +66,11 @@ static void printHelp(void) {
   printf("    -v, --version    prints version information\n");
   printf("    -h, --help       prints this help text\n");
   printf("    -o, --log-file FILE\n");
-  printf("                     location of the file to write log messages to\n");    
+  printf("                     location of the file to write log messages to\n");
   printf("                     Default: %s\n", DEFAULT_LOG_FILE);
   printf("        --pid FILE   location to write the pid to when running as a daemon\n");
   printf("                     Default: %s\n\n", DEFAULT_PID_FILE);
-  
+
   printf(" Classification Options:\n");
   printf("    -n, --worker-threads N\n");
   printf("                     number of threads for processing jobs\n");
@@ -86,9 +86,9 @@ static void printHelp(void) {
   printf("        --missing-item-timeout N\n");
   printf("                     Number of seconds to wait for missing items to be added\n");
   printf("                     before a job depending on them is canceled and an error\n");
-  printf("                     is returned instead");
-  printf("                     Default: %i seconds", DEFAULT_MISSING_ITEM_TIMEOUT);
-  
+  printf("                     is returned instead\n");
+  printf("                     Default: %i seconds\n\n", DEFAULT_MISSING_ITEM_TIMEOUT);
+
   printf(" Item Cache Options:\n");
   printf("        --db FILE    location of the item cache database file\n");
   printf("        --create     if provide the classifier with create the database at\n");
@@ -105,7 +105,7 @@ static void printHelp(void) {
   printf("        --min-tokens N\n");
   printf("                     the minimum number of tokens an item requires to be\n");
   printf("                     classified\n\n");
-  
+
   printf(" HTTP Options:\n");
   printf("    -p, --port N     the port to run the HTTP server on\n");
   printf("    -a, --allowed_ip IP_ADDRESS\n");
@@ -121,23 +121,23 @@ void termination_handler(int sig) {
   } else {
     termination_in_progress = 1;
     fprintf(stderr, "Terminating classifier...\n");
-    
+
     if (httpd) {
       fprintf(stderr, "\tShutting down httpd.\n");
       httpd_stop(httpd);
     }
-    
+
     if (engine) {
       fprintf(stderr, "\tShutting down engine.\n");
       ce_stop(engine);
       free_classification_engine(engine);
     }
-    
+
     if (item_cache) {
       fprintf(stderr, "\tClosing database.\n");
       free_item_cache(item_cache);
     }
-    
+
     fprintf(stderr, "Complete.\n");
     exit(sig);
   }
@@ -146,7 +146,7 @@ void termination_handler(int sig) {
 static void _daemonize(const char * pid_file) {
   int pid, sid;
   pid = fork();
-      
+
   if (pid < 0) {
     exit(EXIT_FAILURE);
   } else if (pid > 0) {
@@ -162,24 +162,24 @@ static void _daemonize(const char * pid_file) {
     // exit the foreground process
     exit(EXIT_SUCCESS);
   }
-  
+
   umask(0);
-  
+
   sid = setsid();
   if (sid < 0) {
     fprintf(stderr, "setsid failed: %s", strerror(errno));
     exit(EXIT_FAILURE);
   }
-  
+
   if (chdir("/") < 0) {
     fprintf(stderr, "chdir(\"/\") failed: %s", strerror(errno));
     exit(EXIT_FAILURE);
   }
 }
 
-static int start_classifier(const char * db_file, const char * tokenizer_url) {  
+static int start_classifier(const char * db_file, const char * tokenizer_url) {
   SET_XML_ERROR_HANDLERS;
-  
+
   if (CLASSIFIER_OK != item_cache_create(&item_cache, db_file, &item_cache_options)) {
     fprintf(stderr, "Error opening classifier database file at %s: %s\n", db_file, item_cache_errmsg(item_cache));
     free_item_cache(item_cache);
@@ -190,11 +190,11 @@ static int start_classifier(const char * db_file, const char * tokenizer_url) {
     item_cache_start_feature_extractor(item_cache);
     item_cache_start_cache_updater(item_cache);
     item_cache_start_purger(item_cache, 60 * 60 * 24);
-    
+
     tagger_cache = create_tagger_cache(item_cache, &tagger_cache_options);
     tagger_cache->tag_retriever = &fetch_url;
     tagger_cache->tag_index_retriever = &fetch_url;
-    
+
     info("Fetching tag index for the first time...");
     Array *tags = NULL;
     char *errmsg = NULL;
@@ -205,9 +205,9 @@ static int start_classifier(const char * db_file, const char * tokenizer_url) {
       error("Error fetching tag index: %s", errmsg);
       free(errmsg);
     }
-    
+
     engine = create_classification_engine(item_cache, tagger_cache, &ce_options);
-    httpd = httpd_start(&http_config, engine, item_cache, tagger_cache);  
+    httpd = httpd_start(&http_config, engine, item_cache, tagger_cache);
     ce_run(engine);
     return EXIT_SUCCESS;
   }
@@ -225,36 +225,36 @@ int main(int argc, char **argv) {
   item_cache_options.cache_update_wait_time = DEFAULT_CACHE_UPDATE_WAIT_TIME;
   item_cache_options.load_items_since = DEFAULT_LOAD_ITEMS_SINCE;
   item_cache_options.min_tokens = DEFAULT_MIN_TOKENS;
-  
+
   int longindex;
   int opt;
   static struct option long_options[] = {
       {"version", no_argument, 0, 'v'},
       {"help", no_argument, 0, 'h'},
-      
+
       {"log-file", required_argument, 0, 'o'},
       {"pid", required_argument, 0, PID_VAL},
-      {"db", required_argument, 0, DB_VAL},      
+      {"db", required_argument, 0, DB_VAL},
       {"create-db", no_argument, 0, CREATE_DB_VAL},
-      
+
       {"cache-update-wait-time", required_argument, 0, CACHE_UPDATE_WAIT_TIME_VAL},
       {"load-items-since", required_argument, 0, LOAD_ITEMS_SINCE_VAL},
       {"tokenizer-url", required_argument, 0, TOKENIZER_URL_VAL},
       {"min-tokens", required_argument, 0, MIN_TOKENS_VAL},
       {"missing-item-timeout", required_argument, 0, MISSING_ITEM_TIMEOUT_VAL},
-      
+
       {"worker-threads", required_argument, 0, 'n'},
       {"positive-threshold", required_argument, 0, 't'},
       {"performance-log", required_argument, 0, PERFORMANCE_LOG_FILE_VAL},
-      
+
       {"port", required_argument, 0, 'p'},
       {"allowed_ip", required_argument, 0, 'a'},
-      
+
       {"tag-index", required_argument, 0, TAG_INDEX_VAL},
-      
+
       {0, 0, 0, 0}
   };
-  
+
   while (-1 != (opt = getopt_long(argc, argv, SHORT_OPTS, long_options, &longindex))) {
     switch (opt) {
       case 'o':
@@ -268,11 +268,11 @@ int main(int argc, char **argv) {
         break;
       case CREATE_DB_VAL:
         create_database = true;
-        break;      
+        break;
       case 'd':
         daemonize = true;
         break;
-      
+
       /* Item Cache Options */
       case CACHE_UPDATE_WAIT_TIME_VAL:
         item_cache_options.cache_update_wait_time = strtol(optarg, NULL, 10);
@@ -286,7 +286,7 @@ int main(int argc, char **argv) {
       case TOKENIZER_URL_VAL:
         tokenizer_url = optarg;
         break;
-        
+
       /* Classification Engine Options */
       case 'n': /* Number of worker threads */
         ce_options.worker_threads = strtol(optarg, NULL, 10);
@@ -300,20 +300,20 @@ int main(int argc, char **argv) {
       case MISSING_ITEM_TIMEOUT_VAL:
         ce_options.missing_item_timeout = strtol(optarg, NULL, 10);
         break;
-        
+
       /* HTTP options */
       case 'p':
         http_config.port = strtol(optarg, NULL, 10);
         break;
       case 'a':
         http_config.allowed_ip = optarg;
-        break;  
-        
+        break;
+
       /* Tagger Cache options */
       case TAG_INDEX_VAL:
         tagger_cache_options.tag_index_url = optarg;
         break;
-        
+
       /* Common Options */
       case 'h':
         // TODO Add help
@@ -329,9 +329,9 @@ int main(int argc, char **argv) {
       break;
     }
   }
-      
+
   int rc = EXIT_SUCCESS;
-  
+
   if (create_database) {
     char error[512];
     if (CLASSIFIER_OK != item_cache_initialize(db_file, error)) {
@@ -345,28 +345,28 @@ int main(int argc, char **argv) {
       fprintf(stderr, "Could not create %s: %s\n", log_file, strerror(errno));
       exit(EXIT_FAILURE);
     }
-    
+
     if (NULL == realpath(log_file, real_log_file)) {
       fprintf(stderr, "Could not find %s: %s\n", real_log_file, strerror(errno));
       exit(EXIT_FAILURE);
     }
-    
+
     if (NULL == realpath(db_file, real_db_file)) {
       fprintf(stderr, "Could not find %s: %s\n", real_db_file, strerror(errno));
       exit(EXIT_FAILURE);
     }
-          
+
     if (daemonize) {
       _daemonize(pid_file);
     }
-    
+
     if (signal(SIGINT, termination_handler) == SIG_IGN)  signal(SIGINT, SIG_IGN);
     if (signal(SIGHUP, termination_handler) == SIG_IGN)  signal(SIGHUP, SIG_IGN);
     if (signal(SIGTERM, termination_handler) == SIG_IGN) signal(SIGTERM, SIG_IGN);
-    
+
     initialize_logging(real_log_file);
     rc = start_classifier(real_db_file, tokenizer_url);
   }
-  
+
   return rc;
 }
